b132e9cebb4bc4b3eea70d3c2b57b54facfdcc86
libp <924701358@qq.com>
2021-10-18 21:39:25 +0800

    二分查找本质在于不断缩短左右边界的距离

--- algorithm/binarysearch.py
+++ algorithm/binarysearch.py
@@ -18,6 +18,33 @@ class Solution:
                 low = mid + 1
         return -1
 
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left,right=1,n
+        while left < right:
+            mid = (right-left)//2 + left
+            # if isBadVersion(mid):
+            if mid > 4 :
+                right = mid
+            else:
+                left= mid + 1
+            
+        return left
+    
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left,right=0,len(nums)-1
+        while left <= right:
+            mid = (right+left)//2 
+            if (nums[mid]>target):
+                right = mid - 1
+            elif (nums[mid] < target):
+                left = mid + 1
+            else:
+                return mid
+        return left;
 
 
 nums,target = [-1,0,3,5,9,12],9
